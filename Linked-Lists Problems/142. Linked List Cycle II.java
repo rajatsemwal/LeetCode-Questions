79bb4df05be5b50f36eab27cd2097e320ea6267a
Rajat Semwal <66676396+heisenberg609@users.noreply.github.com>
2021-02-20 00:12:01 +0530

    Create 142. Linked List Cycle II.java

---
+++ Linked-Lists Problems/142. Linked List Cycle II.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode intersection(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                return slow;
+            }
+        }
+        return null;
+    }
+    public ListNode detectCycle(ListNode head) {
+        
+        if(head == null || head.next == null)
+            return null;
+        
+        ListNode intersect = intersection(head);
+        if(intersect == null)
+            return null;
+        
+        ListNode start = head;
+        while(intersect != start) {
+            start = start.next;
+            intersect = intersect.next;
+        }
+        return start;
+    }
+}
