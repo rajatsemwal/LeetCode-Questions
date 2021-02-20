b081ddf758b0ff2fad56f0203f80e93b6dd0faf1
Rajat Semwal <66676396+heisenberg609@users.noreply.github.com>
2021-02-20 21:47:00 +0530

    Update 88. Merge Sorted Array.java

--- Arrays Problems/88. Merge Sorted Array.java
+++ Arrays Problems/88. Merge Sorted Array.java
@@ -1,19 +1,18 @@
-public class Solution {
-    public void merge(int A[], int m, int B[], int n) {
- 
-        while(m > 0 && n > 0){
-            if(A[m-1] > B[n-1]){
-                A[m+n-1] = A[m-1];
-                m--;
-            }else{
-                A[m+n-1] = B[n-1];
-                n--;
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int p1 = m-1, p2 = n-1, i = m+n-1;
+        
+        while(p2 >= 0) {
+            
+            if(p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[i] = nums1[p1];
+                i--; p1--;
             }
-        }
- 
-        while(n > 0){
-            A[m+n-1] = B[n-1];
-            n--;
+            else {
+                nums1[i] = nums2[p2];
+                i--; p2--;
+            }
         }
     }
 }
