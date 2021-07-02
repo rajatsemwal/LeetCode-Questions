5b318069b12030e792c11d41c0f546d8dbf67651
Rajat Semwal <66676396+rajatsemwal@users.noreply.github.com>
2021-07-02 14:05:59 +0530

    Update 160. Intersection of Two Linked Lists.java

--- Linked-Lists Problems/160. Intersection of Two Linked Lists.java
+++ Linked-Lists Problems/160. Intersection of Two Linked Lists.java
@@ -9,6 +9,8 @@
  *     }
  * }
  */
+
+// This approach uses auxilliary space O(n) as HashSet. But if you need to solve it in O(1) space, follow another approach given below
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         Set<ListNode> seen = new HashSet<>();
@@ -27,3 +29,54 @@ public class Solution {
         return null;
     }
 }
+
+
+/*
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        int len1 = 0, len2 = 0;
+        ListNode nodeA = headA; ListNode nodeB = headB;
+        
+        while(nodeA != null) {
+            
+            len1++; nodeA = nodeA.next;
+        }
+        
+        while(nodeB != null) {
+            
+            len2++; nodeB = nodeB.next;
+        }
+        nodeA = headA; nodeB = headB;
+        
+        if(len1 > len2) {
+            
+            int diff = len1 - len2;
+            
+            while(diff > 0) {
+                nodeA = nodeA.next;
+                diff--;
+            }
+        }
+        else {
+            
+            int diff = len2 - len1;
+            while(diff > 0) {
+                nodeB = nodeB.next;
+                diff--;
+            }
+        }
+        
+        while(nodeA != null && nodeB != null) {
+            
+            if(nodeA == nodeB)
+                return nodeA;
+            
+            nodeA = nodeA.next;
+            nodeB = nodeB.next;
+        }
+        
+        return null;
+    }
+}
+*/
