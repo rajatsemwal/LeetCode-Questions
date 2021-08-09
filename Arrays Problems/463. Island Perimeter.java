eb53c0edd07589695dffd32ce60806d5fad69deb
Rajat Semwal <66676396+rajatsemwal@users.noreply.github.com>
2021-08-10 00:04:04 +0530

    Update 463. Island Perimeter.java

--- Arrays Problems/463. Island Perimeter.java
+++ Arrays Problems/463. Island Perimeter.java
@@ -1,28 +1,29 @@
-/*The intuition behind the solution is that whenever you encounter an island or 1, increase the perimter, because at that time, we are checking only for that
-single island's perimeter. After that, check whether the current island has any neighbouring island before it or not(in previous row and column). If there exists
-any neighbour, decreement the perimeter ny 2 sides, because, whenever we will merge 2 squares, we will lose 2 sides.*/
+/*The intuition behind the solution is that whenever you encounter an island or 1, increase the perimter by 4, because at that time, we are checking only for
+that single island's perimeter. After that, check whether the current island has any neighbouring island before it or not(in previous row and column).
+If there exists any neighbour in previous rows and colums, decreement the perimeter by 2 sides respectvely, for each previous row and column, 
+because, whenever we will merge 2 squares, we will lose 2 sides.*/
 
 
 class Solution {
     public int islandPerimeter(int[][] grid) {
         
-        int islands = 0;
+        int perimeter = 0;
         
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[i].length; j++) {
                 
                 if(grid[i][j] == 1) {
                     
-                    islands += 4;
+                    perimeter += 4;
                     
                     if(i > 0 && grid[i-1][j] == 1)
-                        islands -= 2;
+                        perimeter -= 2;
                     if(j > 0 && grid[i][j-1] == 1)
-                        islands -= 2;
+                        perimeter -= 2;
                 }
             }
         }
         
-        return islands;
+        return perimeter;
     }
 }
